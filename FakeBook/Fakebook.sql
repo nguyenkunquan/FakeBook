48c5873bbcb870f3415e5b20f7d9caa7ee2fa949
HoangDepTrai <nguyenhuyhoang77qn@gmail.com>
2023-11-24 10:30:05 +0700

    Modified file sql

--- FakeBook/Fakebook.sql
+++ FakeBook/Fakebook.sql
@@ -1,72 +1,95 @@
-create database Fakebook
+﻿-- Tạo cơ sở dữ liệu Fakebook
+CREATE DATABASE IF NOT EXISTS Fakebook;
+USE Fakebook;
 
+-- Tạo bảng USER_Account
+CREATE TABLE USER_Account (
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    user_name NVARCHAR(50) NOT NULL,
+    gender VARCHAR(10),
+    birthdate DATE,
+    phone BIGINT,
+    pass VARCHAR(50) NOT NULL,
+    info_name NVARCHAR(50),
+    first_Name NVARCHAR(50),
+    last_Name NVARCHAR(50),
+    registerTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-USE Fakebook
+-- Tạo bảng FRIENDSHIP
+CREATE TABLE FRIENDSHIP (
+    id_user_1 INT NOT NULL,
+    id_user_2 INT NOT NULL,
+    FOREIGN KEY(id_user_1) REFERENCES USER_Account(id_user),
+    FOREIGN KEY(id_user_2) REFERENCES USER_Account(id_user),
+    PRIMARY KEY(id_user_1, id_user_2)
+);
 
-create table USER_Account(
-	id_user int not null,
-	user_name nvarchar(50) not null,
-	gender varchar(10),
-	birthdate date,
-	phone bigint,
-	pass varchar(50) not null,
-	info_name nvarchar(50),
-	PRIMARY KEY(id_user)
-	)
+-- Tạo bảng POST
+CREATE TABLE POST (
+    id_post INT AUTO_INCREMENT PRIMARY KEY,
+    content NVARCHAR(100),
+    img VARCHAR(10),
+    like_num INT,
+    comment INT,
+    id_user INT NOT NULL,
+    createdTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(id_user) REFERENCES USER_Account(id_user)
+);
 
-create table FRIENDSHIP(
-	id_user_1 int not null,
-	id_user_2 int not null,
-	FOREIGN KEY(id_user_1) REFERENCES USER_Account(id_user),
-	FOREIGN KEY(id_user_2) REFERENCES USER_Account(id_user),
-	PRIMARY KEY(id_user_1, id_user_2)
-	)
+-- Tạo bảng LIKE_status
+CREATE TABLE LIKE_status (
+    id_post INT NOT NULL,
+    id_user INT NOT NULL,
+    FOREIGN KEY(id_post) REFERENCES POST(id_post),
+    FOREIGN KEY(id_user) REFERENCES USER_Account(id_user),
+    PRIMARY KEY(id_post, id_user)
+);
 
-create table POST(
-	 id_post int not null,
-	 content nvarchar(100),
-	 img varchar(10),
-	 like_num int,
-	 comment int,
-	 id_user int not null,
-	 PRIMARY KEY(id_post),
-	 FOREIGN KEY(id_user) REFERENCES USER_Account(id_user)
-	 )
+-- Tạo bảng COMMENT_status
+CREATE TABLE COMMENT_status (
+    id_post INT NOT NULL,
+    id_user INT NOT NULL,
+    content NVARCHAR(50),
+    createdTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(id_post) REFERENCES POST(id_post),
+    FOREIGN KEY(id_user) REFERENCES USER_Account(id_user),
+    PRIMARY KEY (id_post, id_user, content)
+);
 
-
-
-create table LIKE_status(
-	id_post int not null,
-	id_user int not null,
-	FOREIGN KEY(id_post) REFERENCES POST(id_post),
-	FOREIGN KEY(id_user) REFERENCES USER_Account(id_user),
-	PRIMARY KEY(id_post, id_user)
-	)
-
-create table COMMENT_status(
-	id_post int not null,
-	id_user int not null,
-	Content nvarchar(50),
-	FOREIGN KEY(id_post) REFERENCES POST(id_post),
-	FOREIGN KEY(id_user) REFERENCES USER_Account(id_user),
-	PRIMARY KEY(id_post, id_user, Content)
-	)
-
-CREATE TRIGGER IncreaseLikeNum
-ON LIKE_status
+-- Tạo trigger IncreaseCommentNum
+DELIMITER //
+CREATE TRIGGER IncreaseCommentNum
 AFTER INSERT
-AS
+ON COMMENT_status FOR EACH ROW
 BEGIN
-    SET NOCOUNT ON;
+    DECLARE postId INT;
+    SELECT NEW.id_post INTO postId;
 
-    DECLARE @postId INT;
+    IF EXISTS (SELECT 1 FROM POST WHERE id_post = postId) THEN
+        UPDATE POST
+        SET comment = comment + 1
+        WHERE id_post = postId;
+    END IF;
+END;
+//
+DELIMITER ;
 
-    SELECT @postId = id_post FROM inserted;
+-- Tạo trigger IncreaseLikeNum
+DELIMITER //
+CREATE TRIGGER IncreaseLikeNum
+AFTER INSERT
+ON LIKE_status FOR EACH ROW
+BEGIN
+    DECLARE postId INT;
+    SELECT NEW.id_post INTO postId;
 
-    IF EXISTS (SELECT 1 FROM POST WHERE id_post = @postId)
-    BEGIN
+    IF EXISTS (SELECT 1 FROM POST WHERE id_post = postId) THEN
         UPDATE POST
         SET like_num = like_num + 1
-        WHERE id_post = @postId;
-    END
+        WHERE id_post = postId;
+    END IF;
 END;
+//
+DELIMITER ;
+ 
