115fcdf6d5d036e9b56e81075b5bc26dbe6c2560
HoangDepTrai <nguyenhuyhoang77qn@gmail.com>
2023-11-28 23:17:39 +0700

    Add page Fakebook

--- FakeBook/src/main/java/DAO/PostDAO.java
+++ FakeBook/src/main/java/DAO/PostDAO.java
@@ -44,8 +44,30 @@ public class PostDAO implements DAOInterface<Post> {
     }
 
     @Override
-    public int update(Post t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int update(Post post) {
+        try {
+            Connection connection = JDBCUtil.getConnection();
+            String insertQuery = "INSERT INTO POST (content, img, like_num, comment, id_user)"
+                    + "VALUES (?, ?, ?, ?, ?)";
+            // Tạo PreparedStatement
+            PreparedStatement preparedStatement;
+            preparedStatement = connection.prepareStatement(insertQuery);
+            // Thiết lập các tham số trong truy vấn
+            preparedStatement.setString(1, post.getContent());
+            preparedStatement.setString(2, post.getImg());
+            preparedStatement.setInt(3, post.getLikeNum());
+            preparedStatement.setInt(4, post.getCommentNum());
+            preparedStatement.setInt(5, post.getIdUser());
+            preparedStatement.setInt(6, post.getId());
+
+            // Thực hiện INSERT
+            int rowsInserted = preparedStatement.executeUpdate();
+            JDBCUtil.closeConnection(connection);
+            return rowsInserted;
+        } catch (SQLException ex) {
+            System.out.println("error: " + ex.getMessage());
+        }
+        return 0;
     }
 
     @Override
