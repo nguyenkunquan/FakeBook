1654523892ceef63afe46e7b5f697150ceb5ec82
Nguyen-Huy-Phuc <134113309+Nguyen-Huy-Phuc@users.noreply.github.com>
2023-12-04 20:42:07 +0700

    Update LoginService.java

--- FakeBook/src/main/java/Service/LoginService.java
+++ FakeBook/src/main/java/Service/LoginService.java
@@ -4,7 +4,28 @@
  */
 package Service;
 
+import DAO.UserDAO;
+import Model.User;
+import java.util.List;
 
 public class LoginService {
-    
+
+        private UserDAO userDAO;
+
+        public LoginService() {
+            this.userDAO = UserDAO.getInstance();
+        }
+
+        public int insertUser(User user) {
+           List<User> listUser = userDAO.selectAll();
+           if(!listUser.isEmpty()){
+            for (User user1 : listUser) {
+                if(user1.getUsername().equals(user.getUsername())
+                        ||user1.getId().equals(user.getId()))
+                    return 0;
+            }
+           }
+            return userDAO.insert(user);
+        }
+
 }
