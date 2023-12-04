6d8e7affbc57db2da149ca8339af837b6eaf5ea2
Nguyen-Huy-Phuc <134113309+Nguyen-Huy-Phuc@users.noreply.github.com>
2023-12-04 20:33:40 +0700

    add_Login_and_Register

---
+++ FakeBook/src/main/java/Controller/Register.java
@@ -0,0 +1,130 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import Model.User;
+import Service.LoginService;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Date;
+import java.util.Calendar;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author Nguyễn Huy Phúc
+ */
+
+@WebServlet(name = "Register", urlPatterns = {"/Register"})
+@MultipartConfig()
+public class Register extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Register</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Register at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("/views/Login/FormRegister/register_form.jsp");
+        rd.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String id_user = request.getParameter("id_user");
+        String first_name = request.getParameter("first_name");
+        String last_name = request.getParameter("last_name");
+        String user_name = request.getParameter("user_name");
+        String password = request.getParameter("password");
+        String phone = request.getParameter("phone");
+        String email = request.getParameter("email");
+        String day = request.getParameter("daySelect");
+        String month = request.getParameter("monthSelect");
+        String year = request.getParameter("yearSelect");
+        String dateString = year + "-" + month + "-" + day;
+        Date dateOfBirth = Date.valueOf(dateString);
+        String gender = request.getParameter("gender");
+        Part avatarPart = request.getPart("avatar");
+        String folderUpload = "/files";
+        String pathUploadFolder = request.getServletContext().getRealPath(folderUpload);
+        String fileName = Paths.get(avatarPart.getSubmittedFileName()).getFileName().toString();
+        if (!Files.exists(Paths.get(pathUploadFolder))) {
+            Files.createDirectories(Paths.get(pathUploadFolder));
+        }
+        String urlAvatar = pathUploadFolder + "/" + fileName;
+        avatarPart.write(urlAvatar);
+        String avatarInsert = folderUpload + "/" + fileName;
+        User u = new User(id_user, user_name, password, gender,dateOfBirth , phone, email, first_name, last_name, avatarInsert);
+        LoginService loginService = new LoginService();
+        int result = loginService.insertUser(u);
+        if (result > 0) {
+            response.sendRedirect("./Demo");
+            return;
+        }
+        response.sendRedirect("./Register");
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
