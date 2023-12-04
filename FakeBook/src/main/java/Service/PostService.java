ea23800ee8a6f4d577927e0f76c5419fd5c8b1f1
giaphu290 <150423390+giaphu290@users.noreply.github.com>
2023-12-04 23:04:22 +0700

    Update PostService.java

--- FakeBook/src/main/java/Service/PostService.java
+++ FakeBook/src/main/java/Service/PostService.java
@@ -1,81 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Model;
 
-import java.sql.Date;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 
 
-public class Post {
-    private int id;
-    private String content;
-    private String img;
-    private int likeNum;
-    private int commentNum;
-    private int idUser;
+public class PostService {
 
-    public Post() {
+    //connect to db => done
+    //query to db get data
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/blog";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "123456";
+    public Connection connection = null;
+
+    public PostService() {
+        this.connection = getConnection(DB_URL, USER_NAME, PASSWORD);
     }
 
-    public Post(int id, String content, String img, int likeNum, int commentNum, int idUser) {
-        this.content = content;
-        this.img = img;
-        this.likeNum = likeNum;
-        this.commentNum = commentNum;
-        this.idUser = idUser;
-    }
+    /**
+     * get connection to mySql
+     *
+     * @param urlDBString
+     * @param username
+     * @param password
+     * @return
+     */
+    private Connection getConnection(String urlDBString, String username, String password) {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(urlDBString,
+                    username, password);
+            System.out.println("connect to DB success!!");
+        } catch (ClassNotFoundException | SQLException exception) {
+            System.out.println("can't connect to database!!!" + exception.getMessage());
+        }
 
+        return conn;
+    }
+        public int insertCustomer(int id, String content, String img , int likeNum, int commentNum, int idUser) {
+        try {
+            String insertQuery = "INSERT INTO POST (id_post, content, img, like_num, comment_num, user_name) VALUES (?, ?, ?, ?, ?, ?)";
+            // Tạo PreparedStatement
+            PreparedStatement preparedStatement;
+            preparedStatement = connection.prepareStatement(insertQuery);
+            // Thiết lập các tham số trong truy vấn
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2, content);
+            preparedStatement.setString(3,img);
+            preparedStatement.setInt(4, likeNum);
+            preparedStatement.setInt(5, commentNum);
+            preparedStatement.setInt(6, idUser);
 
-    public void setId(int id) {
-        this.id = id;
+            // Thực hiện INSERT
+            int rowsInserted = preparedStatement.executeUpdate();
+            return rowsInserted;
+        } catch (SQLException ex) {
+            System.out.println("error: " + ex.getMessage());
+        }
+        return 0;
     }
-    
-    public int getId() {
-        return id;
-    }
-    
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public int getLikeNum() {
-        return likeNum;
-    }
-
-    public void setLikeNum(int likeNum) {
-        this.likeNum = likeNum;
-    }
-
-    public int getCommentNum() {
-        return commentNum;
-    }
-
-    public void setCommentNum(int commentNum) {
-        this.commentNum = commentNum;
-    }
-
-    public int getIdUser() {
-        return idUser;
-    }
-
-    public void setIdUser(int idUser) {
-        this.idUser = idUser;
-    }
-    
-    
 }
