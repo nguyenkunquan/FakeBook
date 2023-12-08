948819cdbcec6964bcf3df5f7ed4d5270718a17e
giaphu290 <150423390+giaphu290@users.noreply.github.com>
2023-12-08 11:36:28 +0700

    Upload Like Model

---
+++ FakeBook/src/main/java/Model/Like.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author MSI ADMIN
+ */
+public class Like {
+        private String user_name;
+        private String postID;
+
+    public Like() {
+    }
+
+    public Like(String user_name, String postID) {
+        this.user_name = user_name;
+        this.postID = postID;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPostID() {
+        return postID;
+    }
+
+    public void setPostID(String postID) {
+        this.postID = postID;
+    }
+        
+    
+}
